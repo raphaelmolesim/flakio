e919fcad98d94e5e374d3612774d74480822bfe7
Raphael Molesim <raphael.molesim@gmail.com>
2023-10-20 14:15:12 -0300

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .post("/add", ({body}) => {
+    console.log(body.username);
+    return JSON.stringify(body);
+  })
+  .post("/validatebody", ({body}) => body , {    
+    body: t.Object({
+      id: t.Number(),
+      username: t.String()
+    })
+  })
+  .get("id/:id", (context) => {
+    console.log("You are the id:", context.params.id);
+    return {
+      "id": context.params.id
+    }
+  })
+  .get("/query", ({query}) => {
+    return {
+      "id": query.id,
+      "name": query.name
+    }
+  })
+  .post("/transform", ({body}) => body, {
+    transform: ({body}) => {
+      body.id = body.id + 1
+    }
+  })
+  .decorate("youDecor", () => Date.now())
+  .state("counter", 10)
+  .get("/mystate", ({
+    youDecor,
+    store: {counter}
+  }) => `${counter} ${youDecor()}`)  
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
